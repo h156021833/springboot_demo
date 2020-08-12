8affbc894f8240cf7048ca052eeb4831559a6ef6
paul <983067893@qq.com>
2020-08-12 14:22:43 +0800

    springboot-demo-2

--- src/main/java/cn/kgc/springboot_demo/ServletInitializer.java
+++ src/main/java/cn/kgc/springboot_demo/ServletInitializer.java
@@ -9,7 +9,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(SpringbootDemoApplication.class);
         //1
-        // 2
+        //2
     }
 
 }
